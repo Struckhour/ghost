6119d2d0607f5d18f79038a924e1a93e4bc83ee9
Struckhour <96839301+Struckhour@users.noreply.github.com>
2022-03-26 23:56:42 -0300

    first scope

--- src/components/Wildlands.vue
+++ src/components/Wildlands.vue
@@ -1,14 +1,21 @@
 <template>
-    <body class="bg-slate-900 w-screen h-screen">
+    <body class="bg-slate-900 w-screen h-screen text-white">
         <img src="../assets/wildlands.jpg" class="w-96 h-20 m-auto rounded-md object-cover">
-        <div class="h-[600px] w-[800px] m-auto mt-8 border border-red-700 grid grid-cols-3">
-            <ul class="text-white">
-                <h3 class="text-lg underline">Scopes</h3>
-                <li>scopey scopey</li>
-                <li>scopey 2</li>
-            </ul>
-            <img src="../assets/scope.jpg" class="h-[600px] w-[400px] object-cover m-auto">
+        <div class="h-[90%]">
+            <div class="h-[80%] w-[100%] absolute m-auto mt-8 border border-red-700 grid grid-cols-3">
+                <ul class="">
+                    <h3 class="text-lg underline">Scopes</h3>
+                    <li>T5XI</li>
+                    <li>scopey 2</li>
+                </ul>
+                <img src="../assets/scope.jpg" class="h-[100%] w-[100%] object-cover m-auto">
+                <div class="absolute bg-green-900 px-2 rounded-lg text-lg top-[51%] left-[44%]">150m</div>
+                <div class="absolute bg-green-900 px-2 rounded-lg text-lg top-[54.5%] left-[51%]">200m</div>
+                <div class="absolute bg-green-900 px-2 rounded-lg text-lg top-[58%] left-[44%]">250m</div>
+                <div class="absolute bg-green-900 px-2 rounded-lg text-lg top-[61%] left-[51%]">375m</div>
+            </div>
         </div>
+
     </body>
 
 </template>
