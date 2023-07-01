5b077075cc7836e3f763fe1200b4445cbf06bcb7
HeavyHawk <HeavyHawkmanship@gmail.com>
2023-07-01 13:24:00 -0700

    adds search bar to riflemenu

--- src/components/Riflemenu.vue
+++ src/components/Riflemenu.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
     import { ref, computed } from 'vue'
     import sezzing from '../assets/sezzing';
+    
+    
+    const searchQuery = ref('');
+    const showSearchBar = ref(false);
 
 
     const props = defineProps<{ rifles: string[], rifleTitle: string, show2: boolean, scopeMenu: boolean, selectedScope:string, gameName:string}>()
@@ -16,6 +20,8 @@
       emit('rifleMenuClicked', shower.value)
       scopeMenu.value = false;
       emit('scopeMenu', scopeMenu.value);
+    // Toggle showSearchBar when clicking on the dropdown toggle button
+      showSearchBar.value = shower.value;
     }
 
     const rifleTitle1 = computed(() => {
@@ -32,14 +38,21 @@
         window.scrollTo({ top: 0, behavior: "smooth" });
     }
 
+    const computedRifles = computed(() => {
+    const query = searchQuery.value.toLowerCase();
+      return props.rifles.filter((rifle) => 
+        rifle.toLowerCase().includes(query));
+    });
+
+
 </script>
 
 <template>
     <!-- Dropdown toggle button -->
   <button
       @click="isOpen2"
-      class="flex justify-end mr-0 px-1 text-cyan-300 bg-slate-900 hover:text-cyan-200 hover:bg-opacity-[90%] bg-opacity-80 rounded-md border-slate-600 border"
-  >
+      class="flex justify-end mr-0 px-1 text-cyan-300 bg-slate-900 hover:text-cyan-200 hover:bg-opacity-[90%] bg-opacity-80 rounded-md border-slate-600 border" style="font-family: ;">
+      
       <span class="mr-4">{{rifleTitle1.value}}</span>
       <svg
       class="w-5 h-5 text-cyan-300"
@@ -55,17 +68,31 @@
       </svg>
   </button>
 
+    <!-- Search bar -->
+    <input
+    v-if="showSearchBar"
+    v-model="searchQuery"
+    type="text"
+    class="w-full px-2 py-0 mt-1 text-cyan-300 uppercase bg-black border border-gray-300 rounded-md focus:outline-none focus:border-cyan-300"
+    style="font-family: ;"
+    placeholder="Search..."
+  />
+
   <!-- Dropdown menu -->
-  <div class="grid grid-cols-1 md:w-[13rem] w-[15rem] bg-slate-900 bg-opacity-10 -translate-x-[0rem] z-40 absolute">
+  <div id="riflemenu" class="grid grid-rows-30 grid-cols-1 md:grid-cols-2 md:w-[27rem] w-[16rem] bg-slate-900 bg-opacity-10 -translate-x-[0rem] z-40 absolute" style="font-family: ;">
+
       <div
       v-show="props.show2"
-      v-for="(rifle) in rifles" :key="rifle"
+      
+      v-for="(rifle) in computedRifles"
+
+      :key="rifle"
       class="right-0 py-0 mt-0 rounded-sm shadow-xl shadow-black bg-slate-900 bg-opacity-80"
       >
-        <!-- <div v-if="sezzing[gameName][selectedScope].hasOwnProperty(rifle)" @click="changeRifle(rifle)" class="cursor-pointer px-4 md:py-0 py-2 md:my-0 my-1  z-40 md:text-[.85rem] text-base text-yellow-400 font-bold hover:bg-yellow-400 hover:text-black">
+        <div v-if="sezzing[gameName][selectedScope].hasOwnProperty(rifle)" @click="changeRifle(rifle)" class="cursor-pointer px-4 md:py-0 py-2 md:my-0 my-1  z-40 md:text-[.85rem] text-base text-yellow-400 font-bold hover:bg-yellow-400 hover:text-black">
           {{rifle}}
-        </div> -->
-        <div @click="changeRifle(rifle)" class="cursor-pointer px-4 md:py-0 py-2 md:my-0 my-1  z-40 md:text-[.85rem] text-base text-cyan-300 hover:bg-cyan-700 hover:text-indigo-100">
+        </div>
+        <div @click="changeRifle(rifle); showSearchBar=false" class="cursor-pointer px-4 md:py-0 py-2 md:my-0 my-1  z-40 md:text-[.85rem] text-base text-cyan-300 hover:bg-cyan-700 hover:text-indigo-100">
           {{rifle}}
         </div>
       </div>
